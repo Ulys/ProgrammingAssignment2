76895e71723bec55a3acdb7bea4ddab73637a986
Anton Razumovskyy <anton.razumovskyy@gmail.com>
2014-10-25 18:54:14 +0300

    added memoization of matrix functions

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,56 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+## @function Create object which contain matrix and could memorize invert matrix
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  
+  solve_mat <- NULL;
+  
+  set <- function (y) {
+    
+    x <<- y;
+    solve_mat <<- NULL;
+  }
+  
+  get <- function () {  
+    x;
+  }
+  
+  setSolve <- function (solve) {
+    solve_mat <<- solve
+  }
+  
+  getSolve <<- function() {
+    solve_mat;
+  }
+  
+  list(
+    set = set,
+    get = get,
+    setSolve = setSolve,
+    getSolve = getSolve
+  );
 }
 
 
-## Write a short comment describing this function
+## @function return inverse matrix if it count previously if NOT it count it save and return
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  ## Return a matrix that is the inverse of 'x'
+  solve_mat <- x$getSolve();
+  
+  if(!is.null(solve_mat)) {
+    
+    message('getting cached data')
+    return (solve_mat)
+  }
+  
+  data <- x$get();
+  
+  solve_mat <- solve(data);
+  
+  x$setSolve(solve_mat);
+  
+  solve_mat
 }
